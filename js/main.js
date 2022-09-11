7b4b151ee037c2ffc25dae3b624c2e27adc7d321
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2022-09-12 00:57:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,89 @@
+//btn for scroll
+
+let btnup = document.getElementById('btnup')
+
+
+
+btnup.onclick = function() {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  })
+}
+
+
+//end button for scroll
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ ** Increase Numbers On Scrolling
+ ** Video URL: https://youtu.be/PLsUdgLnzgQ
+ */
+
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".our-skills");
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+
+  //btn for scroll
+  if (window.scrollY >= 200) {
+    btnup.style.display = "block";
+  } else {
+    btnup.style.display = "none";
+  }
+  // Skills Animate Width
+  if (window.scrollY >= section.offsetTop - 250) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+  // Stats Increase Number
+  if (window.scrollY >= statsSection.offsetTop-350) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
